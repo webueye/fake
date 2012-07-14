043b9a0cb5e552720e8ac36a4c46df7a94778fc4
webueye <webueye@gmail.com>
2012-07-14 10:36:44 +0800

    u

--- src/main/java/com/taoists/code/controller/BarCodeBean.java
+++ src/main/java/com/taoists/code/controller/BarCodeBean.java
@@ -1,10 +1,5 @@
 package com.taoists.code.controller;
 
-import java.util.List;
-
-import net.sf.json.JSONArray;
-
-import com.google.common.collect.Lists;
 
 /**
  * @author rubys@vip.qq.com
@@ -13,30 +8,13 @@ import com.google.common.collect.Lists;
 public class BarCodeBean {
 
 	private String pic;
-	
-	public BarCodeBean(){
-		
+
+	public BarCodeBean() {
+
 	}
-	
-	public BarCodeBean(String pic){
+
+	public BarCodeBean(String pic) {
 		this.pic = pic;
-	}
-
-	public static void main(String[] args) {
-
-		List<String> list = Lists.newArrayList("a", "b", "c");
-		p(JSONArray.fromObject(list).toString());
-
-		BarCodeBean b1 = new BarCodeBean();
-		BarCodeBean b2 = new BarCodeBean();
-		BarCodeBean b3 = new BarCodeBean();
-		b1.pic = "http://a.com";
-		b2.pic = "http://b.com";
-		b3.pic = "http://c.com";
-
-		List<BarCodeBean> beans = Lists.newArrayList(b1, b2, b3);
-		p(JSONArray.fromObject(beans).toString());
-
 	}
 
 	public void setPic(String pic) {
@@ -45,10 +23,6 @@ public class BarCodeBean {
 
 	public String getPic() {
 		return pic;
-	}
-
-	static void p(Object value) {
-		System.err.println(value);
 	}
 
 }
